2997614800d55270334feba7c34e1ae02999038f
YusiZhang <yusi.leo.zhang@gmail.com>
2014-07-07 14:39:54 -0400

    add new classes

--- DSProj3/src/node/MasterMain.java
+++ DSProj3/src/node/MasterMain.java
@@ -1,15 +1,23 @@
 package node;
+
+import java.net.SocketAddress;
+import java.util.concurrent.ConcurrentHashMap;
+
 /*
  * For dfs, master should do following things
  * handled by (master) scheduler:
- * 1. receive file info from client, update slave table
+ * 1. receive files info from client, update slave table
  * and reply layout info to client.
  * 2. find files
  * 3. heartbeat slave
  * 4. upload file
- * 
  */
 public class MasterMain {
+	
+	//table for slave info : socketAddr-slaveInfo
+	public static ConcurrentHashMap<SocketAddress,SlaveInfo> slavePool = new ConcurrentHashMap<SocketAddress, SlaveInfo>();
+	
+	//
 	public static void main(String[] args) {
 		
 	}
