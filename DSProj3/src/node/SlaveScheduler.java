dffca8104da21686639555b5e7fb7f83d0276c39
YusiZhang <yusi.leo.zhang@gmail.com>
2014-07-09 21:47:21 -0400

    reconstruct slave scheduler add master heart beat

---
+++ DSProj3/src/node/SlaveScheduler.java
@@ -0,0 +1,113 @@
+package node;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+import communication.Message;
+import config.ParseConfig;
+
+public class SlaveScheduler extends Thread {
+	// public static int curPort = ParseConfig.StartPort;
+	public static int slaveId = 0;
+	ServerSocket listener;
+	Socket socket;
+	Message msg;
+	ParseConfig conf;
+
+	public SlaveScheduler(int port) throws IOException {
+		listener = new ServerSocket(port);
+		socket = null;
+		msg = null;
+		conf = SlaveMain.conf;
+	}
+
+	public void run() {
+		try {
+
+		} catch (Exception e) {
+			System.out.println("Master main port listener is wrong "
+					+ e.toString());
+			System.exit(0);
+		}
+
+		while (true) {
+
+			try {
+				socket = listener.accept();
+				msg = Message.receive(socket);
+				System.out.println("the scheduler receives a " + msg.getType()
+						+ " messge");
+
+			} catch (Exception e) {
+				System.out.println("There is no message " + e.toString());
+				continue;
+			}
+
+			switch (msg.getType()) {
+
+			case FILE_PUT_REQ_TO_SLAVE:
+				System.out.println("Message received from "
+						+ socket.getRemoteSocketAddress()
+						+ " type: FILE_PUT_REQ_TO_SLAVE; content: "
+						+ msg.getContent().toString());
+				SlaveMain.curPort++;
+
+				Message reply = new Message(Message.MSG_TYPE.PORT,
+						SlaveMain.curPort);
+
+				// tell the client which slaves are available
+				try {
+					reply.send(socket);
+					System.out.println("send the reply to client "
+							+ reply.getContent());
+					System.out
+							.println("send the port number from the slave to the client");
+
+					// Scheduler s1 = new Scheduler(SlaveMain.curPort);
+					// System.out.println("listen on the current port "+SlaveMain.curPort);
+					// s1.start();
+					ServerSocket slaveListener = new ServerSocket(
+							SlaveMain.curPort);
+					Socket soc = null;
+					soc = slaveListener.accept();
+					Message fileMsg = null;
+					fileMsg = Message.receive(soc);
+					System.out.println("File received from "
+							+ fileMsg.getType()
+							+ fileMsg.getContent().toString());
+
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				break;
+			case KEEP_ALIVE:
+				break;
+
+			default:
+				break;
+			}
+
+			try {
+				socket.close();
+				listener.close();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	private void receiveFile() {
+		// TODO Auto-generated method stub
+
+	}
+}
